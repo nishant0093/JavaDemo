420b575289f88423175d11204a0202d58abcaf9a
nishant0093 <nishant0093@gmail.com>
2023-01-18 20:53:00 +0530

    Java Demo project

--- src/com/demo/Demo.java
+++ src/com/demo/Demo.java
@@ -1,5 +1,34 @@
 package com.demo;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Demo {
+	
+	public static void demo() {
+		ArrayList<Character> list = new ArrayList<Character>();
+		HashSet<Character> set = new HashSet<Character>();
+		
+		String str = "Hello world we love java";
+		char[] ch = str.replace(" ", "").toCharArray();
+		
+		for(int i = 0; i<ch.length; i++) {
+			if(set.contains(ch[i])) {
+				list.add(ch[i]);
+			}
+			else {
+				set.add(ch[i]);
+			}	
+		}
+		System.out.println("Duplicate letters are: "+list);	
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		String s = "Hello all we invite";
+		System.out.println();
+	}
 
 }
